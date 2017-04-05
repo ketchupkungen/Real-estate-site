3e1edf6e430f315a715d096ffc177408b9bd0708
Anton Johansson <anton.xdd@gmail.com>
2017-04-05 14:49:14 +0200

    import from /contactform-contactview.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { HeaderComponent } from './header.component';
 import { FooterComponent } from './footer.component';
+import { ContactFormContactViewComponent } from './contactform-contactview.component';
 import { ObjectHeaderComponent } from './object-header.component';
 import { SearchComponent } from './search.component';
 import { ObjectSmallComponent } from './object-small.component';
@@ -19,7 +20,7 @@ import { ObjectSmallComponent } from './object-small.component';
   	AppComponent,
   	HeaderComponent,
   	FooterComponent,
-    //ContactFormContactViewComponent,
+    ContactFormContactViewComponent,
     ObjectHeaderComponent,
     SearchComponent,
   	ObjectSmallComponent
