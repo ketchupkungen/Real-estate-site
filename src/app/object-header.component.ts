a6d2424042c6e0c7260be1bb35ee396d9d190426
Martin Lennervald <martin.lennervald@gmail.com>
2017-04-05 13:37:56 +0200

    Added nav-pills and stylesUrls to component object-header

--- src/app/object-header.component.ts
+++ src/app/object-header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'object-header',
   templateUrl: 'templates/object-header.html',
+  styleUrls: ['app/object-header.component.css']
 })
 export class ObjectHeaderComponent {
     activeLink: string = 'Översikt';
