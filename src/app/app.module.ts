cbce187346e389b21874afbfab19430ead200ce6
Freddy Persson <FP_turtle@hotmail.com>
2017-04-05 10:00:30 +0200

    Implementing bootstrap in Angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }      from '@angular/core';
+import { NgbModule } 		 from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, NgbModule.forRoot() ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
