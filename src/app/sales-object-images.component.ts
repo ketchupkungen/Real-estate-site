8fd046063245a3e718618aac09b3867609717ad8
Martin Lennervald <martin.lennervald@gmail.com>
2017-04-10 20:18:08 +0200

    Adde skeleton for salesObjectImages-tab

---
+++ src/app/sales-object-images.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sales-object-images',
+    templateUrl: 'templates/sales-object-images.html',
+    styleUrls: ['css/sales-object-images.css']
+})
+
+export class SalesObjectImagesComponent {
+
+}
