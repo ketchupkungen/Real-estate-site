b95fabcdda3add382c49fb48e24f9f6ed714f834
Victor Glimskog <victor.glimskog@gmail.com>
2017-04-10 09:36:52 +0200

    Fixed wrong css-file linking. Fixed ngFor rendering options in select box. Refactored code from search.component.css into search-component.css

--- src/app/sell-with-us.component.ts
+++ src/app/sell-with-us.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'sell-with-us',
-  templateUrl: 'templates/sell-with-us-component.html'
+  templateUrl: 'templates/sell-with-us-component.html',
+  styleUrls: ['css/sell-with-us.css']
 })
 export class SellWithUsComponent  { 
-	types: [
+	types = [
 		'Alla bostadstyper',
 		'Bostadsrätt',
 		'Villa',
