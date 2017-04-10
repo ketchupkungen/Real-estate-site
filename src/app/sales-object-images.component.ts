0595a74c1de4145d18aa96090e2a0c389e3b3947
Martin Lennervald <martin.lennervald@gmail.com>
2017-04-10 20:40:56 +0200

    Added skeleton for carousel in SalesObjectImagesComponent for viewing images to a specific saleObject

--- src/app/sales-object-images.component.ts
+++ src/app/sales-object-images.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'sales-object-images',
     templateUrl: 'templates/sales-object-images.html',
-    styleUrls: ['css/sales-object-images.css']
+    styleUrls: ['css/sales-object-images.css'],
+    providers: [NgbCarouselConfig]
 })
 
 export class SalesObjectImagesComponent {
-
+    constructor(config: NgbCarouselConfig) {
+        // customize default values of carousels used by this component tree
+        config.interval = 10000;
+        config.wrap = true;
+        config.keyboard = true;
+    }
 }
