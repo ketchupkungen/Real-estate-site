b7faafd37fc8f05f5b571dd9b42ee9bbc4d402a6
Anton Johansson <anton.xdd@gmail.com>
2017-04-17 13:03:54 +0200

    Imported data from usData.json Reduced CSS code and used bootstrap mt/mb(Tack Freddy). Design fixes and positions.

--- src/app/contact-company.component.ts
+++ src/app/contact-company.component.ts
@@ -9,10 +9,24 @@ import { Http } 			  from '@angular/http';
 
 export class CompanyCardComponent {
 
-  usObject:any;
+  companyName:any;
+  companyPhone:any;
+  companyMailHq:any;
+  companyMailSupport:any;
+
+  companyCity:any;
+  companyAddress:any;
+  companyPostcode:any;
 
   constructor(private http:Http) {
-    this.http.get('data/us.json')
-      .subscribe(res => this.usObject = res.json().name);
+    this.http.get('data/us.json').subscribe(res => this.companyName = res.json().name);
+
+    this.http.get('data/us.json').subscribe(res => this.companyPhone = res.json().contact.phone);
+    this.http.get('data/us.json').subscribe(res => this.companyMailHq = res.json().contact.mail.hq);
+    this.http.get('data/us.json').subscribe(res => this.companyMailSupport = res.json().contact.mail.support);
+
+    this.http.get('data/us.json').subscribe(res => this.companyCity = res.json().place.city);
+    this.http.get('data/us.json').subscribe(res => this.companyAddress = res.json().place.adress);
+    this.http.get('data/us.json').subscribe(res => this.companyPostcode = res.json().place.postcode);
   }
 }
