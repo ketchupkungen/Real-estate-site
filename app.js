3d4ed64c4fdaa55b438d64e8714b264a23fda466
Freddy Persson <FP_turtle@hotmail.com>
2017-04-20 08:58:41 +0200

    More mongoose stuff added and also added so that the sales object data gets created

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var express = require('express');
 
 // Mongoose added
 var fs = require('fs');
+var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+require('mongoosefromclass')(mongoose);
 
 // Create a new express server, store in the variable app
 var app = express();
 
+// Make the express server abl.e to read the body of requests
+app.use(bodyparser.json({ limit: '5mb' }));
+app.use(bodyparser.urlencoded({ extended: false }));
+
 //Fake JSON Data
-// var salesData = require('./src/data/salesData.json');
-// var brokersData = require('./src/data/brokersData.json');
-// var usData = require('./src/data/usData.json');
+var salesData = require('./src/data/sales-data.json');
+var brokersData = require('./src/data/brokers-data.json');
+var usData = require('./src/data/us-data.json');
 
 // Mongoose added
 global.mongoose = mongoose;
@@ -21,6 +27,24 @@ global.mongoose = mongoose;
 // (takes away the warning "mpromise is deprecated")
 mongoose.Promise = Promise;
 
+// Load classes, make them global and then convert selected ones to modules
+var classesToLoad = {
+	Sale: 'module'
+	// Broker: 'module',
+	// Us: 'module'
+};
+
+for(let className in classesToLoad) {
+	let pathName = './modules/' + className.toLowerCase() + '.class';
+	global[className] = require(pathName);
+}
+
+for(let className in classesToLoad) {
+	if(classesToLoad[className] == 'module') {
+		global[className] = mongoose.fromClass(global[className]);
+	}
+}
+
 // Point to folders where we have static files
 // (our frontend code)
 app.use(express.static('src'));
@@ -28,6 +52,14 @@ app.use(express.static('./'));
 
 // Other routes go here
 // ...
+
+// Never cache request starting with "/rest/"
+app.use((req, res, next)=>{
+	if(req.url.indexOf('/rest/') >= 0) {
+		res.set("Cache-Control", "no-store, must-revalidate");
+	}
+	next();
+});
 
 // If no other route rule fulfilled then return index.html
 app.get('*',(req, res)=>{
@@ -45,4 +77,50 @@ function onceConnected() {
 	app.listen(3000, ()=>{
 	  console.log('Express app listening on port 3000!');
 	});
+
+	// For each collection type we have JSON of
+	// If the db counts 0 of either item
+	// It will insert the JSON into the db.
+	createFakeDataFromJSON();
+}
+
+function createFakeDataFromJSON() {
+    Sale.count(function(err, count) {
+        if (count === 0) {
+            createDeafultSales();
+        }
+    });
+
+    // Broker.count(function(err, count) {
+    // 	if (count === 0) {
+    // 		createDefaultBroker();
+    // 	}
+    // });
+
+    // Us.count(function(err, count) {
+    //     if (count === 0) {
+    //         createDeafultUs();
+    //     }
+    // });
+
+	function createDeafultSales() {
+		salesData.forEach(function(data) {
+			new Sale(data).save();
+		});
+	}
+
+	// function createDefaultBrokers() {
+	// 	thingsLeftToSave += brokersData.length;
+
+	// 	brokersData.forEach(function(education) {
+	// 		new Broker(brokers).save();
+	// 	});
+	// }
+
+	// function createDeafultUs() {
+	// 	thingsLeftToSave += usData.length;
+
+	// 	new Us(usData).save();
+	// }
+
 }
---
+++ src/app/rest.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { RestEntity } from "../class/rest/rest-entity.class";
+
+@Injectable()
+export class RestService {
+
+  constructor(private http: Http) { 
+  }
+
+  newRestEntity(entityName:string){
+    let restEntity = new RestEntity(entityName);
+    restEntity.http = this.http;
+    return restEntity;
+  }
+
+}
