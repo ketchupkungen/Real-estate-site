36c74e70aae5446308946c0090a2885d84312586
Martin Lennervald <martin.lennervald@gmail.com>
2017-04-21 11:14:03 +0200

    Now reading brooker data from us.json

--- src/app/sales-object-contact.component.ts
+++ src/app/sales-object-contact.component.ts
@@ -1,5 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } 		from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/switchMap';
+
+import { SalesObjectService } from './sales-object.service';
+import { SalesObject } 	from '../class/sales-object.class';
 
 @Component ({
   selector: 'sales-object-contact',
@@ -8,11 +15,32 @@ import { Http } from '@angular/http';
 })
 
 export class SalesObjectContactComponent {
+  private salesObject: SalesObject;
+  private us: any;
+  private brooker: Object;
 
-  constructor (private http: Http) {
-    this.http.get('data/us.json').subscribe();  
-  }
-  ngOnInit () {
+	constructor(
+		private salesObjectService: SalesObjectService,
+		private route: ActivatedRoute,
+		private location: Location,
+    private http: Http
+	) {}
 
-  }
+	ngOnInit(): void {
+    // Get content of us.json 
+    this.http.get('./data/us.json')
+        .map(res => res.json())
+        .subscribe(us => this.us = us,
+          err => console.log(err));
+
+    // Find the amenable brooker for clicked salesObject
+    this.route.params
+			.switchMap((params: Params) => this.salesObjectService.getObject(+params['id']))
+			.subscribe((object: any) => {
+        this.salesObject = object;
+        this.brooker = this.us.sellers[this.salesObject.sellerId];
+        console.log(this.salesObject);
+        console.log(this.brooker);
+      });
+	}
 }
