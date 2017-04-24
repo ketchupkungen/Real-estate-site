38346916c3044e21d1d493a652dd34cd8813a106
Freddy Persson <FP_turtle@hotmail.com>
2017-04-24 14:29:00 +0200

    Brokers is now in the database and the rest is working on this data also

--- modules/Broker.class.js
+++ modules/Broker.class.js
@@ -1,19 +1,15 @@
 module.exports = class Broker {
   schema() {
     return {
-      sellers: [
-        {
-          id: {type: Number},
-          firstName: {type: String},
-          lastName: {type: String},
-          contact: {
-            phone: String,
-            mail: String,
-            personDesc: String
-          },
-          profile_image_src: {type: String}
-        }
-      ]
+      id: {type: Number},
+      firstName: {type: String},
+      lastName: {type: String},
+      contact: {
+        phone: String,
+        mail: String,
+        personDesc: String
+      },
+      profile_image_src: {type: String}
     };
   }
 };
